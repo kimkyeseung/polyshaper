a6dfe793dfc3f27df4abd94a16b7a890e33481ab
김계승 <>
2018-11-01 20:38:05 +0900

    upload test pass

--- src/components/test/Upload.spec.js
+++ src/components/test/Upload.spec.js
@@ -18,7 +18,6 @@ function setup() {
     }
   };
 }
-//윈도우 객체 가짜로 만들어서 애드이벤트리스너 가짜로 만들고, 드래그 이벤트 호출되었는지 테스트.
 
 describe('Upload component', () => {
   it('has states correctly', () => {
@@ -50,29 +49,34 @@ describe('Upload component', () => {
       const { component } = setup();
       expect(global.window.addEventListener).toHaveBeenCalledWith('drop', component.instance().handleDrop);
     });
-
-    it('remove event dragover', () => {
-      const { component } = setup();
-      expect(global.window.removeEventListener).toHaveBeenCalledWith('dragover', component.instance().dropTarget);
-    });
-
-    it('remove event dragleave', () => {
-      const { component } = setup();
-      expect(global.window.removeEventListener).toHaveBeenCalledWith('dragleave', component.instance().dropLeave);
-    });
-
-    it('remove event drop', () => {
-      const { component } = setup();
-      expect(global.window.removeEventListener).toHaveBeenCalledWith('drop', component.instance().handleDrop);
-    });
   });
 
-  describe('window should remove listen event on', () => {
+  describe('window should remove event listeners on componentWillUnmount', () => {
     beforeEach(() => {
       const { window } = new JSDOM('<!doctype html><html><body></body></html>');
       window.removeEventListener = jest.fn();
       global.window = window;
     });
 
+    it('remove event dragover', () => {
+      let { component } = setup();
+      const dropTarget = component.instance().dropTarget;
+      component.unmount();
+      expect(global.window.removeEventListener).toHaveBeenCalledWith('dragover', dropTarget);
+    });
+
+    it('remove event dragleave', () => {
+      const { component } = setup();
+      const dropLeave = component.instance().dropLeave;
+      component.unmount();
+      expect(global.window.removeEventListener).toHaveBeenCalledWith('dragleave', dropLeave);
+    });
+
+    it('remove event drop', () => {
+      const { component } = setup();
+      const handleDrop = component.instance().handleDrop;
+      component.unmount();
+      expect(global.window.removeEventListener).toHaveBeenCalledWith('drop', handleDrop);
+    });
   });
 });
